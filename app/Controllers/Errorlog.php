1427bbf903bab58018e683201d6c368e1a8b33ba
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2022-03-22 15:26:58 +0700

    fix: errorlog

--- app/Controllers/Errorlog.php
+++ app/Controllers/Errorlog.php
@@ -6,7 +6,7 @@ class Errorlog extends BaseController
 	public function index($dateNow = "") {
 
         $dateNow = $dateNow == "" ? date("Y-m-d") : $dateNow;
-        $dateNow = '2022-03-21';
+        // $dateNow = '2022-03-21';
         $path = WRITEPATH."logs/log-".$dateNow.".log";
         // echo $path;exit();
         if(file_exists($path)){
