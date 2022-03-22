0735993813ff66765be0bb9a7631148fc65b7f31
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2022-03-22 19:57:12 +0700

    fix: production2

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -70,6 +70,26 @@ class Database extends Config
         'port'     => 3306,
     ];
 
+    public $production2 = [
+        'DSN'      => '',
+        'hostname' => 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'username' => 'h0l6wuogjttnj3sd',
+        'password' => 'rjf3jn25dmop11sf',
+        'database' => 'n859sygoomxxq3m4',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => (ENVIRONMENT !== 'production2'),
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 3306,
+    ];
+
     /**
      * This database connection is used when
      * running PHPUnit database tests.
@@ -107,6 +127,8 @@ class Database extends Config
             $this->defaultGroup = 'tests';
         }else if(ENVIRONMENT === 'production'){
             $this->defaultGroup = 'production';
+        }else if(ENVIRONMENT === 'production2'){
+            $this->defaultGroup = 'production2';
         }
     }
 }
