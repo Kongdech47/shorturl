032587bec51d186eb6e2d5cddd5af36b60c0fe10
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2022-03-22 18:51:05 +0700

    fix: conn db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -50,6 +50,26 @@ class Database extends Config
         'port'     => 3306,
     ];
 
+    public $production = [
+        'DSN'      => '',
+        'hostname' => 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'username' => 'l61pchrw2nifck33',
+        'password' => 'ko8btkwy6z0kzh3c',
+        'database' => 'gepvy7rdanp95q83',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => (ENVIRONMENT !== 'production'),
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 3306,
+    ];
+
     /**
      * This database connection is used when
      * running PHPUnit database tests.
@@ -85,6 +105,8 @@ class Database extends Config
         // we don't overwrite live data on accident.
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
+        }else if(ENVIRONMENT === 'production'){
+            $this->defaultGroup = 'production';
         }
     }
 }
